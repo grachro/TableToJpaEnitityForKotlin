056c41b77e849cf0eb3b29368333d707852b7e75
grachro <grachro@gmail.com>
2016-07-27 19:21:13 +0900

    抽象クラス

--- src/main/kotlin/com/grachro/t2entity/MetaColumn.kt
+++ src/main/kotlin/com/grachro/t2entity/MetaColumn.kt
@@ -2,69 +2,77 @@ package com.grachro.t2entity
 
 import com.google.common.base.CaseFormat
 
-data class MetaColumn(val name:String, val type:String, val nullable:Boolean, var pk:Boolean) {
+data class MetaColumn(val name: String, val type: String, val nullable: Boolean, val autoIncrement: Boolean, var pk: Boolean) {
 
-    val field:String
-    get() {
-        return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, this.name.toUpperCase())
-    }
-
-    val entityType:ColumnType = ColumnType.get(this.type)
-
-    val kotlinPrefix:String
-    get() {
-        return when(this.entityType) {
-            ColumnType.INT -> "var"
-            else -> if (this.nullable ) "var" else "lateinit var"
+    val field: String
+        get() {
+            return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, this.name.toUpperCase())
         }
 
-    }
+    val entityType: ColumnType = ColumnType.get(this.type)
+
+    val kotlinPrefix: String
+        get() {
+            return if (this.entityType.primitive) {
+                "var"
+            } else {
+                if (this.nullable) "var" else "lateinit var"
+            }
+        }
 
 
-    val kotlinSuffix:String
+    val kotlinSuffix: String
         get() {
 
             val nullable = if (this.nullable) "?" else ""
             val defaultValue = if (this.nullable) " = null" else ""
 
-            return when(this.entityType) {
+            return when (this.entityType) {
                 ColumnType.INT -> "Int${kotlinIntDefalult}" //Int or Int? = 0
                 ColumnType.STRING -> "String${nullable}${defaultValue}"//String or String?
-                ColumnType.TIMESTAMP -> "Timestamp${nullable}${defaultValue}"//String or String?
+                ColumnType.BOOLEAN -> "Boolean${kotlinBooleanDefalult}"//Boolean or Boolean?
+                ColumnType.TIMESTAMP -> "Timestamp${nullable}${defaultValue}"//Timestamp or String?
                 else -> "${this.type}${nullable}"
             }
         }
 
-    val kotlinIntDefalult:String
-    get() {
-        return if (this.nullable) "? = null" else " = 0"
-    }
+    val kotlinIntDefalult: String
+        get() {
+            return if (this.nullable) "? = null" else " = 0"
+        }
 
-    val kotlinStringDefalult:String
+    val kotlinStringDefalult: String
         get() {
             return if (this.nullable) "? = null" else " = \"\""
         }
 
-    val kotlinPkSuffix:String
+    val kotlinBooleanDefalult: String
+        get() {
+            return if (this.nullable) "? = null" else " = false"
+        }
+
+    val kotlinPkSuffix: String
         get() {
 
             val defaultValue = if (this.nullable) " = null" else ""
 
-            return when(this.entityType) {
+            return when (this.entityType) {
                 ColumnType.INT -> "Int${kotlinIntDefalult}" //Int = 0
                 ColumnType.STRING -> "String${kotlinStringDefalult}"//String = ""
-                ColumnType.TIMESTAMP -> "Timestamp${nullable}${defaultValue}"//String or String?
+                ColumnType.TIMESTAMP -> "Timestamp${nullable}${defaultValue}"
+                ColumnType.TIMESTAMP -> "Boolean${kotlinBooleanDefalult}"
                 else -> "${this.type}${nullable}"
             }
         }
 
-    val kotlinDefaultValue:String
+    val kotlinDefaultValue: String
         get() {
 
-            return when(this.entityType) {
+            return when (this.entityType) {
                 ColumnType.INT -> "0"
                 ColumnType.STRING -> "\"\""
                 ColumnType.TIMESTAMP -> "Timestamp(0)"
+                ColumnType.BOOLEAN -> "false"
                 else -> "\"\""
             }
         }
--- src/main/kotlin/com/grachro/t2entity/TableSchemaLoader.kt
+++ src/main/kotlin/com/grachro/t2entity/TableSchemaLoader.kt
@@ -1,17 +1,23 @@
 package com.grachro.t2entity
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.sql.DatabaseMetaData
 import java.sql.DriverManager
+import java.sql.ResultSetMetaData
 import java.util.*
 
 object TableSchemaLoader {
 
-    fun load(url:String,driver:String,props:Properties, fromTableName:String): MetaTable {
+    private val LOG = LoggerFactory.getLogger(TableSchemaLoader::class.java)
+
+
+    fun load(url: String, driver: String, props: Properties, fromTableName: String): MetaTable {
         Class.forName(driver)
-        val con =  DriverManager.getConnection(url, props)
+        val con = DriverManager.getConnection(url, props)
         try {
-            val primaryKyeNames = getPrimaries(con.metaData,fromTableName)
-            val cols = getCols(con.metaData,fromTableName,primaryKyeNames)
+            val primaryKyeNames = getPrimaries(con.metaData, fromTableName)
+            val cols = getCols(con.metaData, fromTableName, primaryKyeNames)
 
             return MetaTable(fromTableName, primaryKyeNames, cols)
         } finally {
@@ -20,7 +26,7 @@ object TableSchemaLoader {
 
     }
 
-    private fun getPrimaries(meta: DatabaseMetaData, tableName:String ):Set<String> {
+    private fun getPrimaries(meta: DatabaseMetaData, tableName: String): Set<String> {
         var result = HashSet<String>()
         val pkRm = meta.getPrimaryKeys(null, null, tableName)
         try {
@@ -33,18 +39,68 @@ object TableSchemaLoader {
         return result
     }
 
-    private fun getCols(meta: DatabaseMetaData, tableName:String,primaryKyeNames:Set<String> ):List<MetaColumn> {
+    private fun getCols(meta: DatabaseMetaData, tableName: String, primaryKyeNames: Set<String>): List<MetaColumn> {
         var result = ArrayList<MetaColumn>()
         val rs = meta.getColumns(null, null, tableName, null)
         try {
+
+
+            val cols = arrayOf("TABLE_CAT",
+                    "TABLE_SCHEM",
+                    "TABLE_NAME",
+                    "COLUMN_NAME",
+                    "DATA_TYPE",
+                    "TYPE_NAME",
+                    "COLUMN_SIZE",
+                    "BUFFER_LENGTH",
+                    "DECIMAL_DIGITS",
+                    "NUM_PREC_RADIX",
+                    "NULLABLE",
+                    "REMARKS",
+                    "COLUMN_DEF",
+                    "SQL_DATA_TYPE",
+                    "SQL_DATETIME_SUB",
+                    "CHAR_OCTET_LENGTH",
+                    "ORDINAL_POSITION",
+                    "IS_NULLABLE",
+                    "SCOPE_CATALOG",
+                    "SCOPE_SCHEMA",
+                    "SCOPE_TABLE")
+
+
             while (rs.next()) {
                 val colName = rs.getString("COLUMN_NAME")
                 val type = rs.getString("TYPE_NAME")
                 val isNullable = rs.getBoolean("IS_NULLABLE")
+                val isAutoIncrement = rs.getBoolean("IS_AUTOINCREMENT")
                 val pk = primaryKyeNames.contains(colName)
-                result.add(MetaColumn(colName, type, isNullable, pk))
+                result.add(MetaColumn(colName, type, isNullable, isAutoIncrement, pk))
 
+
+                //*******************
+                //debug
+                //*******************
+                if(rs.isFirst) {
+                    LOG.debug("******************")
+                    LOG.debug("******************")
+                    val resultMeta: ResultSetMetaData = rs.metaData
+                    for (i in 1..resultMeta.columnCount) {
+                        LOG.debug("resultMeta.getColumnName(${i})=${resultMeta.getColumnName(i)}")
+                    }
+                    LOG.debug("******************")
+                    LOG.debug("******************")
+                }
+
+                LOG.debug("rs.getString(IS_AUTOINCREMENT)=${rs.getString("IS_AUTOINCREMENT")}")
+                for (col in cols) {
+                    LOG.debug("rs.getString(${col})=${rs.getString(col)}")
+                }
+                LOG.debug("==================")
             }
+
+
+            LOG.debug("==================")
+            LOG.debug("==================")
         } finally {
             rs.close()
         }
